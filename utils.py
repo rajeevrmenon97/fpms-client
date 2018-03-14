c4a806bf412ae339d90dfadffc59b6acc4c183b1
SSL Admin <Rajeev R Menon>
2018-03-14 16:02:44 +0530

    fetch fpData

--- utils.py
+++ utils.py
@@ -4,13 +4,50 @@ import subprocess
 from subprocess import STDOUT
 from pathlib import Path
 import sys
+import os
 
 
 class fpms:
+
     def __init__(self):
         self.probe = ""
         self.gallery = ""
         self.score = 0
+        self.connection = None
+
+    def connectDb(self):
+        try:
+            self.connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='ssl',
+                             db='db_ams',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+        except Exception as e:
+            print('Error connecting to db! \n {!r}, errno is {}'.format(e, e.args[0]))
+
+    def getFingerprintData(self):
+
+        self.connectDb()
+
+        fileList = os.listdir('./fpData')
+        for fileName in fileList:
+            os.remove("./fpData/"+fileName)
+
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "SELECT `id_no`,`fpData` FROM `registered`"
+                cursor.execute(sql)
+                result = cursor.fetchall()
+                for row in result:
+                    with open('fpData/' + row['id_no'] + '.xyt','w') as f:
+                        f.write(row['fpData'].decode('unicode_escape').rstrip())
+
+        except Exception as e:
+            print('Error getting fingerprint Data! \n {!r}, errno is {}'.format(e, e.args[0]))
+
+        finally:
+            self.connection.close()
 
     def register(self, id_no, name, phone, email, fpTemplate):
 
@@ -19,33 +56,30 @@ class fpms:
             print("Cannot find fingerprint template!")
             return
 
-        connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='ssl',
-                             db='db_ams',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+        self.connectDb()
 
         with open(fpTemplate, 'rb') as f:
             blob = f.read()
 
         try:
-            with connection.cursor() as cursor:
+            with self.connection.cursor() as cursor:
                 sql = "SELECT MAX(`serial_no`) FROM `registered`"
                 cursor.execute(sql)
                 result = cursor.fetchone()
                 sno = result["MAX(`serial_no`)"]
+                if sno is None:
+                    sno = 0
                 sno = sno + 1
-            with connection.cursor() as cursor:
+            with self.connection.cursor() as cursor:
                 sql = "INSERT INTO `registered` (`serial_no`, `name`, `id_no`, `phone_no`, `email_id`, `fpData`) VALUES (%s, %s, %s, %s, %s, %s)"
                 cursor.execute(sql, (sno, name, id_no, phone, email, blob, ))
-            connection.commit()
+            self.connection.commit()
 
-        except:
-            print("Error inserting into db")
+        except Exception as e:
+            print('Error inserting into DB!\n {!r}, errno is {}'.format(e, e.args[0]))
 
         finally:
-            connection.close()
+            self.connection.close()
 
     def readFingerprint(self, fileName):
         try:
@@ -54,8 +88,3 @@ class fpms:
             result = exc.output
         result = result.decode('unicode_escape').rstrip()
         print(result)
-
-if __name__ == '__main__':
-    obj = fpms()
-    obj.register("10101","Rajeev",8547088868,"r2m@gmail.com", "f.xyt")
-    #obj.readFingerprint("f.bmp")
