e23a18378129f8ecee51b6a915a5c08c472cfdb2
Rajeev R Menon <rajeevrmenon97@live.com>
2018-03-19 00:02:03 +0530

    create main window

--- amsClient.py
+++ amsClient.py
@@ -41,6 +41,26 @@ class mainWindow(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
 
+        font = "-family Impact -size 20 -weight normal -slant roman "  \
+            "-underline 0 -overstrike 0"
+
+        self.verifyButton = tk.Button(self, command=lambda: controller.show_frame(fpWindow))
+        self.verifyButton.place(relx=0.33, rely=0.33, height=86, width=177)
+        self.verifyButton.configure(activebackground="#d9d9d9")
+        self.verifyButton.configure(text='''Verify''')
+        self.verifyButton.configure(width=177)
+
+        self.registerButton = tk.Button(self, command=lambda: controller.show_frame(registerWindow))
+        self.registerButton.place(relx=0.33, rely=0.62, height=86, width=177)
+        self.registerButton.configure(activebackground="#d9d9d9")
+        self.registerButton.configure(text='''Register''')
+
+        self.title = tk.Message(self)
+        self.title.place(relx=0.03, rely=0.11, relheight=0.13, relwidth=0.94)
+        self.title.configure(font=font)
+        self.title.configure(text='''Attendance Management System''')
+        self.title.configure(width=563)
+
 
 
 class registerWindow(tk.Frame):
